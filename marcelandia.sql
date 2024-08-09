ff97f65fa90ec797d5563d984726e5db1a8bce77
Monteiro <prof.emiliano@gmail.com>
2024-08-09 18:51:15 -0400

    Update marcelandia.sql

--- marcelandia.sql
+++ marcelandia.sql
@@ -1,8 +1,4 @@
-﻿#
-# Structure for table "alunos"
-#
-
-CREATE TABLE `alunos` (
+﻿CREATE TABLE `alunos` (
   `IdAluno` int NOT NULL AUTO_INCREMENT,
   `Nome` varchar(50) DEFAULT NULL,
   `Telefone` varchar(50) DEFAULT NULL,
@@ -13,15 +9,7 @@ CREATE TABLE `alunos` (
   PRIMARY KEY (`IdAluno`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf16le COMMENT='tabela de alunos';
 
-#
-# Data for table "alunos"
-#
-
 INSERT INTO `alunos` VALUES (1,'ana','5555-1234',10000.00,NULL,NULL,NULL),(2,'beth','5554-4321',5678.00,NULL,NULL,NULL),(3,'joão','5553-1234',8956.00,NULL,NULL,NULL),(4,'paulo','55-555-1234',11435.00,NULL,NULL,NULL),(5,'maria','5555-0000',9653.00,NULL,NULL,NULL);
-
-#
-# Structure for table "pets"
-#
 
 CREATE TABLE `pets` (
   `IdPet` int NOT NULL AUTO_INCREMENT,
@@ -36,10 +24,6 @@ CREATE TABLE `pets` (
   KEY `idx_pets` (`IdAluno`),
   CONSTRAINT `idx_pets` FOREIGN KEY (`IdAluno`) REFERENCES `alunos` (`IdAluno`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16le;
-
-#
-# Data for table "pets"
-#
 
 INSERT INTO `pets` VALUES (1,'gato1',1,NULL,NULL,NULL,NULL,NULL),(2,'gato2',2,NULL,NULL,NULL,NULL,NULL),(3,'cao1',3,NULL,NULL,NULL,NULL,NULL),(4,'cao2',2,NULL,NULL,NULL,NULL,NULL),(5,'cao3',2,NULL,NULL,NULL,NULL,NULL),(6,'cao4',3,NULL,NULL,NULL,NULL,NULL),(7,'gato3',1,NULL,NULL,NULL,NULL,NULL);
 
